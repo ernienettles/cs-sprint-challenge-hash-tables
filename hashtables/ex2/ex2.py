eb411311c11e54e179134e04877faa9f1db7aff5
Ernie Nettles <ernienettles@gmail.com>
2020-10-02 13:45:20 -0400

    MVP Met

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,39 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    table = {}
+    route = []
 
+    # Takes the ticket source and makes it the key, and the destination the value
+    for ticket in tickets:
+        table[ticket.source] = ticket.destination
+
+    # This sets current to the value of t['NONE'], which would be 'LAX'.
+    current = table['NONE']
+
+    # While the value of current is not 'NONE', this will append the next destination in the route to the route array.
+    while current != 'NONE':
+        route.append(current)
+        current = table[current]
+    route.append('NONE')
     return route
+
+
+    '''
+    Visualization
+
+    t = {
+        'NONE': 'LAX',
+        'LAX': 'SFO',
+        'SFO': 'BHM',
+        'BHM': 'FLG',
+        'FLG': 'XNA',
+        'XNA': 'CID',
+        'CID': 'SLC',
+        'SLC': 'PIT',
+        'PIT': 'ORD',
+        'ORD': 'NONE'
+    }
+
+    '''
